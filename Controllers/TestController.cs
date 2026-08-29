1ba0997017706448a73870f5b2537d6786e168c7
Yaryna Laiter <jasja200411@gmail.com>
2026-08-29 19:43:44 +0300

    feat: add async and blocking endpoints for starvation testing

---
+++ Controllers/TestController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EngineeringLabs.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : Controller
+{
+    private int goodCounter = 0;
+    private int badCounter = 0;
+    private async Task<string> TestAsyncMethod(string message)
+    {
+        await Task.Delay(1000);
+        
+        return message;
+    }
+    
+    [HttpGet("good")]
+    public async Task<string> GoodAsyncMethod()
+    {
+        return await TestAsyncMethod($"Request g {++goodCounter}");
+    }
+
+    [HttpGet("bad")]
+    public async Task<string> BadasyncMethod()
+    {
+        return TestAsyncMethod($"Request b {badCounter}").Result;
+    }
+}
